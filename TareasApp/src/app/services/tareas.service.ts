58bd10835054d19f0df9eaccb4f0bf38f476fca9
JoseRamonHernandez <67391682+JoseRamonHernandez@users.noreply.github.com / joseramonhernandezmunoz20@gmail.com>
2020-11-27 16:21:54 -0600

    otro repositorio del mismo de ionic

---
+++ TareasApp/src/app/services/tareas.service.ts
@@ -0,0 +1,69 @@
+import { Injectable } from '@angular/core';
+import { Lista } from '../models/lista.model';
+import { ListasComponent } from '../components/listas/listas.component';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class TareasService{
+    
+
+    listas: Lista[] = [];
+
+
+    constructor() {
+        
+        this.cargarStorage();
+
+        /*const lista1 = new Lista('Recolectar cosas');
+        const lista2 = new Lista('Ya fue hom´s');
+
+
+        this.listas.push(lista1, lista2);*/
+
+        
+     }
+
+crearLista(titulo: string){
+    const nuevaLista = new Lista(titulo);
+    this.listas.push(nuevaLista);
+    this.guardarStorage();
+
+    return nuevaLista.id;
+}
+
+borrarLista(lista: Lista){
+  this.listas =  this.listas.filter( listaData =>{
+        return listaData.id !== lista.id
+    });  
+
+    this.guardarStorage();
+}
+
+obtenerLista(id: string | number){
+    id = Number(id);
+
+    this.listas.find(listaData =>{
+        return listaData.id == id;
+    });
+}
+
+guardarStorage(){
+
+localStorage.setItem('data', JSON.stringify( this.listas))
+
+}
+
+cargarStorage(){
+    
+    if(localStorage.getItem('data'))
+    {
+    this.listas = JSON.parse(localStorage.getItem('data'));
+    }
+    else
+    {
+        this.listas = [];
+    }
+}
+
+}
